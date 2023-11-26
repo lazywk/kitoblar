bd1249addfd237cef79b500346c9123627e8b647
lazywk <edoniyorbekdev@gmail.com>
2023-11-26 12:12:49 +0500

    remove console logs

--- src/components/route/CheckAuth.js
+++ src/components/route/CheckAuth.js
@@ -2,8 +2,6 @@ import React from 'react'
 import { userRole } from 'store'
 
 export default function CheckAuth({ role, children }) {
-    console.log(role);
-    console.log(userRole);
 
     return role.some(el => el === userRole) ? children : <p>Not found</p>
 }
